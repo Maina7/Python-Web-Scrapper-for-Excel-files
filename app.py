34631d0fb4c04ced2d8c005a6d359a71d88a88d3
Maina <sirmainamwangi2020@gmail.com>
2025-11-13 23:36:17 +0300

    scrap first

---
+++ app.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("SAMPLE_LINK")
+page = requests.get(url)
+soup = BeautifulSoup(page.text,"html")
+
+table = soup.find("table")
+table_titles = soup.find_all("th")
+table_title_text = [title.text for title in table_titles]
+
+df = pd.DataFrame(columns=table_title_text)
+column_daata = table.find_all("tr")
+
+for row in column_daata[1:]:
+    row_data = row.find_all("td")
+    individual_raw_data = [data.text for data in row_data]
+
+    length = len(df)
+    df.loc[length] = individual_raw_data
+
+df.to_csv(r"C:\learn sth new\Projects-Languages\Django\MarketPrices14.csv", index=False)
+
+
